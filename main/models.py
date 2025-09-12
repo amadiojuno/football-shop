ab5944a0d5dcadc6a09ea68aaa74d5d9c1cfd5c4
Amadio Juno <amadio.juno@ui.ac.id>
2025-09-12 23:35:41 +0700

    Tugas Individu 3

--- main/models.py
+++ main/models.py
@@ -7,9 +7,14 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     rating = models.FloatField(default=0.0)
+    views = models.IntegerField(default=0)
     thumbnail = models.URLField()
     category = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
+    
+    def increment_views(self):
+        self.views += 1
+        self.save()
